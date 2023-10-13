3927a099880c6ba7068fadb4257b1105be2c0afa
Nadya <timrova.nadiia1622@vu.cdu.edu.ua>
2023-10-13 20:50:48 +0300

    hw1

---
+++ js/01-gallery.js
@@ -0,0 +1,45 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const listImages = document.querySelector(".gallery");
+console.log(listImages);
+
+const imagesMarkup = createImagesMurkupList(galleryItems);
+listImages.insertAdjacentHTML('beforeend', imagesMarkup);
+listImages.addEventListener('click', openImage);
+
+function createImagesMurkupList(images){
+    return images
+    .map(({original, preview, description}) => {
+        return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`;
+    })
+    .join("");
+}
+
+function openImage (event) {
+event.preventDefault();
+if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+ const largeImageUrl = event.target.dataset.source;
+
+ const instance = basicLightbox.create(`
+        <img src="${largeImageUrl}">
+    `);
+
+  instance.show();
+
+}
+
