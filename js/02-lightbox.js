fbdd187581829b927f057ec687e079580bce16c2
Nadya <timrova.nadiia1622@vu.cdu.edu.ua>
2023-10-14 13:36:41 +0300

    hw 7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
 console.log(galleryItems);
+
+const listImages = document.querySelector(".gallery");
+const imagesMarkup = createImagesMurkupList(galleryItems);
+listImages.insertAdjacentHTML('beforeend', imagesMarkup);
+
+
+function createImagesMurkupList(images){
+    return images
+    .map(({original, preview, description}) => {
+        return `      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+           <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+     </li>`;
+    })
+    .join("");
+}
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionDelay: 250 
+});
